faa1e4844a382abbe1f32862bf702d5f14553057
c3rb3ru5d3d53c <c3rb3ru5d3d53c@protonmail.com>
2021-11-12 22:44:05 -0400

    plugin

--- plugins/binlex_cutter.py
+++ plugins/binlex_cutter.py
@@ -7,28 +7,25 @@ class MyDockWidget(cutter.CutterDockWidget):
     def __init__(self, parent, action):
         super(MyDockWidget, self).__init__(parent, action)
         self.setObjectName("MyDockWidget")
-        self.setWindowTitle("My cool DockWidget")
-
+        self.setWindowTitle("binlex")
         self._label = QLabel(self)
         self.setWidget(self._label)
-
         QObject.connect(cutter.core(), SIGNAL("seekChanged(RVA)"), self.update_contents)
         self.update_contents()
 
     def update_contents(self):
         disasm = cutter.cmd("pd 1").strip()
-
         instruction = cutter.cmdj("pdj 1")
         size = instruction[0]["size"]
-
         self._label.setText("Current disassembly:\n{}\nwith size {}".format(disasm, size))
 
 
 class MyCutterPlugin(cutter.CutterPlugin):
-    name = "My Plugin"
-    description = "This plugin does awesome things!"
-    version = "1.0"
-    author = "1337 h4x0r"
+
+    name = "binlex"
+    description = "Highlights genetic traits from binlex"
+    version = "1.0.0"
+    author = "@c3rb3ru5d3d53c"
 
     def setupPlugin(self):
         pass
