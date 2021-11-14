3276270fee64226dad6f48b4cdea6bbf9ae7ad13
c3rb3ru5d3d53c <c3rb3ru5d3d53c@protonmail.com>
2021-11-14 12:18:17 -0400

    plugin

--- plugins/binlex_cutter.py
+++ plugins/binlex_cutter.py
@@ -1,6 +1,9 @@
 import cutter
 
+import re
 import os
+import tempfile
+import subprocess
 from pathlib import Path
 from functools import partial
 from multiprocessing import Pool
@@ -71,9 +74,9 @@ class Binlex(cutter.CutterDockWidget):
         self.table_traits.setShowGrid(False)
         self.table_traits.verticalHeader().hide()
         self.table_traits.setSelectionBehavior(QAbstractItemView.SelectRows)
-        self.table_traits.setColumnCount(4)
+        self.table_traits.setColumnCount(2)
         self.table_traits.setRowCount(1)
-        self.table_traits.setHorizontalHeaderLabels(['Name', 'Match', 'Address', 'Trait'])
+        self.table_traits.setHorizontalHeaderLabels(['Name', 'Trait'])
         self.table_traits.setSortingEnabled(True)
         self.table_traits.setVerticalScrollMode(QAbstractItemView.ScrollPerPixel)
         self.table_traits.setHorizontalScrollMode(QAbstractItemView.ScrollPerPixel)
@@ -84,7 +87,7 @@ class Binlex(cutter.CutterDockWidget):
 
         # Search Type
         search_type = QComboBox()
-        search_type.addItems(["Name", "Match", "Address", "Trait"])
+        search_type.addItems(["Name", "Trait"])
         layout.addWidget(search_type)
         layout.setAlignment(search_type, Qt.AlignRight | Qt.AlignBottom)
 
@@ -105,6 +108,26 @@ class Binlex(cutter.CutterDockWidget):
 
         self.show()
 
+    def generate_yara_rule(self, traits):
+        cutter.message('[-] binlex generating yara signature...')
+        yara_rule = 'rule binlex {\n\tstrings:\n'
+        for i in range(0, len(traits)):
+            yara_rule = yara_rule + '\t\t${i} = {open}{trait}{closed}\n'.format(i=i,trait=traits[i]['trait'],open='{',closed='}')
+        yara_rule = yara_rule + '\n\tcondition:\n\t\tany of them\n}'
+        cutter.message('[*] binlex yara generation complete')
+        return yara_rule
+
+
+    def yara_scan(self, rule, file_path):
+        cutter.message('[-] binlex starting yara scan...')
+        yara_rule_file = tempfile.NamedTemporaryFile()
+        yara_rule_file.write(str.encode(yara_rule))
+        command = 'yara -w -s {rule} {file_path}'.format(rule=yara_rule_file.name,file_path=file_path)
+        out = subprocess.getoutput(command)
+        yara_rule_file.close()
+        cutter.message('[*] binlex yara scan complete')
+        return out
+
     def load_traits(self):
         directory = QFileDialog.getExistingDirectory(self, 'Select Folder')
         cutter.message("[-] binlex loading traits...")
@@ -116,11 +139,12 @@ class Binlex(cutter.CutterDockWidget):
         pool = Pool(processes=self.threads)
         traits = pool.map(partial(load_traits_worker,), files)
         traits = [item for sublist in traits for item in sublist]
+        yara_rule = self.generate_yara_rule(traits)
+        self.yara_scan(yara_rule, '/usr/bin/ls')
         self.table_traits.setRowCount(len(traits))
         for i in range(0, len(traits)):
             self.table_traits.setItem(i, 0, QTableWidgetItem(traits[i]['name']))
-            self.table_traits.setItem(i, 1, QTableWidgetItem('no'))
-            self.table_traits.setItem(i, 3, QTableWidgetItem(traits[i]['trait']))
+            self.table_traits.setItem(i, 1, QTableWidgetItem(traits[i]['trait']))
         self.show()
         cutter.message("[*] binlex finished loading traits")
 
