96ddf6d7a08bc17209ba6b9df8dc23b619949b71
c3rb3ru5d3d53c <c3rb3ru5d3d53c@protonmail.com>
2021-12-11 13:06:52 -0400

    recursive jbx81

--- include/decompiler_rev.h
+++ include/decompiler_rev.h
@@ -1,4 +1,6 @@
 #include <vector>
+#include <queue>
+
 #include <capstone/capstone.h>
 #include "common.h"
 
@@ -14,112 +16,113 @@
 using namespace std;
 
 namespace binlex {
-    class DecompilerREV: public Common {
-        private:
-            typedef struct{
-                string type;
-                string bytes;
-                string trait;
-                uint edges;
-                uint blocks;
-                uint insns;
-            } trait;
-        public:
-            struct Section {
-                csh handle;
-                cs_err status;
-                uint offset;
-                uint64_t pc;
-                trait *traits;
-                uint traits_count;
-                void *data;
-                size_t data_size;
-                const uint8_t *code;
-                size_t code_size;
-                vector<uint64_t> blocks;
-                vector<uint64_t> functions;
-                vector<uint64_t> visited;
-            };
-            struct Section sections[DECOMPILER_REV_MAX_SECTIONS];
-            DecompilerREV();
-            /**
-            Set up Capstone Decompiler Architecure and Mode
-            @param arch Capstone Decompiler Architecure
-            @param cs_mode Capstone Mode
-            @param index section index
-            */
-            bool Setup(cs_arch arch, cs_mode mode, uint index);
-            /**
-            Collect Operands for Processing
-            @param insn the instruction
-            @param operand_type the operand type
-            @param index the section index
-            @returns bool
-            */
-            bool CollectOperands(cs_insn *insn, int operand_type, uint index);
-            /**
-            Collect Instructions for Processing
-            @param insn the instruction
-            @param index the section index
-            @returns operand type
-            */
-            uint CollectInsn(cs_insn *insn, uint index);
-            /**
-            Collect Immutable Operands
-            @param imm immutable value
-            @param operand_type the operand type
-            @param index the section index
-            */
-            void CollectImm(int64_t imm, int operand_type, uint index);
-            /**
-            Decompiles Target Data
-            @param data pointer to data
-            @param data_size size of data
-            @param data_offset include section offset
-            @param index the section index
-            @returns program counter position
-            */
-            uint Decompile(void *data, size_t data_size, size_t data_offset, uint index);
-            void Seek(uint64_t address, size_t data_size, uint index);
-            /**
-            Allocate Additional Traits
-            @param count number of additional traits to allocate
-            @param index the section index
-            @return bool
-            */
-            /**
-            Checks if the Instruction is an Ending Instruction
-            @param insn the instruction
-            @return bool
-            */
-            bool IsEndInsn(cs_insn *insn);
-            /**
-            Allocate Traits
-            @param count number of traits to allocate
-            @param index the section index
-            @return bool
-            */
-            bool AllocTraits(uint count, uint index);
-            /**
-            Checks if Address was Already Visited
-            @param address address to check
-            @return bool
-            */
-            bool IsVisited(uint64_t address, uint index);
-            /**
-            Pushes Block Address to the Blocks Vector Stack
-            @param address block address
-            @return address
-            */
-            uint64_t PushBlock(uint64_t address, uint index);
-            /**
-            Pushes Function Address to the Functions Vector Stack
-            @param address function address
-            @return address
-            */
-            uint64_t PushFunction(uint64_t address, uint index);
-            //void Seek(uint offset, uint index);
-            ~DecompilerREV();
+    class DecompilerREV : public Common {
+    private:
+        typedef struct {
+            string type;
+            string bytes;
+            string trait;
+            uint edges;
+            uint blocks;
+            uint insns;
+        } trait;
+    public:
+        struct Section {
+            csh handle;
+            cs_err status;
+            uint offset;
+            uint64_t pc;
+            trait* traits;
+            uint traits_count;
+            void* data;
+            size_t data_size;
+            const uint8_t* code;
+            size_t code_size;
+            vector<uint64_t> blocks;
+            vector<uint64_t> functions;
+            map<uint64_t, int> visited;
+            queue<uint64_t> discovered;
+        };
+        struct Section sections[DECOMPILER_REV_MAX_SECTIONS];
+        DecompilerREV();
+        /**
+        Set up Capstone Decompiler Architecure and Mode
+        @param arch Capstone Decompiler Architecure
+        @param cs_mode Capstone Mode
+        @param index section index
+        */
+        bool Setup(cs_arch arch, cs_mode mode, uint index);
+        /**
+        Collect Operands for Processing
+        @param insn the instruction
+        @param operand_type the operand type
+        @param index the section index
+        @returns bool
+        */
+        bool CollectOperands(cs_insn* insn, int operand_type, uint index);
+        /**
+        Collect Instructions for Processing
+        @param insn the instruction
+        @param index the section index
+        @returns operand type
+        */
+        uint CollectInsn(cs_insn* insn, uint index);
+        /**
+        Collect Immutable Operands
+        @param imm immutable value
+        @param operand_type the operand type
+        @param index the section index
+        */
+        void CollectImm(int64_t imm, int operand_type, uint index);
+        /**
+        Decompiles Target Data
+        @param data pointer to data
+        @param data_size size of data
+        @param data_offset include section offset
+        @param index the section index
+        @returns program counter position
+        */
+        uint Decompile(void* data, size_t data_size, size_t data_offset, uint index);
+        void Seek(uint64_t address, size_t data_size, uint index);
+        /**
+        Allocate Additional Traits
+        @param count number of additional traits to allocate
+        @param index the section index
+        @return bool
+        */
+        /**
+        Checks if the Instruction is an Ending Instruction
+        @param insn the instruction
+        @return bool
+        */
+        bool IsEndInsn(cs_insn* insn);
+        /**
+        Allocate Traits
+        @param count number of traits to allocate
+        @param index the section index
+        @return bool
+        */
+        bool AllocTraits(uint count, uint index);
+        /**
+        Checks if Address was Already Visited
+        @param address address to check
+        @return bool
+        */
+        bool IsVisited(uint64_t address, uint index);
+        /**
+        Pushes Block Address to the Blocks Vector Stack
+        @param address block address
+        @return address
+        */
+        uint64_t PushBlock(uint64_t address, uint index);
+        /**
+        Pushes Function Address to the Functions Vector Stack
+        @param address function address
+        @return address
+        */
+        uint64_t PushFunction(uint64_t address, uint index);
+        //void Seek(uint offset, uint index);
+        ~DecompilerREV();
 
     };
 }
