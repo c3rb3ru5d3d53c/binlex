e5b8763da564a28ef5b90247c7a2322c1fbbda3d
c3rb3ru5d3d53c <c3rb3ru5d3d53c@protonmail.com>
2021-12-12 18:20:19 -0400

    recursive edge cases

--- src/decompiler_rev.cpp
+++ src/decompiler_rev.cpp
@@ -69,6 +69,8 @@ bool DecompilerREV::Setup(cs_arch arch, cs_mode mode, uint index) {
     if (sections[index].status != CS_ERR_OK) {
         return false;
     }
+    sections[index].arch = arch;
+    sections[index].mode = mode;
     return true;
 }
 
@@ -85,32 +87,96 @@ int pthread_create(pthread_t *restrict thread,
                           void *(*start_routine)(void *),
                           void *restrict arg);
 */
-                         
-void DecompilerREV::Worker(cs_arch arch, cs_mode mode, int index) {
+
+bool DecompilerREV::Worker(int index) {
     worker myself;
-    bool exit = false;
-    cs_insn* insn = cs_malloc(sections[index].handle);
-    while(true) {
-        //pthread_mutex_lock(&NET_PTHREAD_MUTEX_UPDATE_VICTIMS);
-        if(!sections[index].discovered.empty()) {
-            uint64_t address = sections[index].discovered.front();
-            sections[index].discovered.pop();
-            //pthread_mutex_unlock(&NET_PTHREAD_MUTEX_UPDATE_VICTIMS);
 
-            // Seek()
-            myself.pc = address;
-            myself.code = (uint8_t *)(sections[index].data + address);
-            myself.code_size = sections[index].data_size + address;
+    // Initialize Decompiler
+    myself.error = cs_open(sections[index].arch, sections[index].mode, &myself.handle);
+    if (myself.error != CS_ERR_OK) {
+        return false;
+    }
+    myself.error = cs_option(myself.handle, CS_OPT_DETAIL, CS_OPT_ON);
+    if (myself.error != CS_ERR_OK) {
+        return false;
+    }
 
-            //Decompile Logic
+    // Setup Decompiler Loop
+    cs_insn *insn = cs_malloc(myself.handle);
+    uint64_t tmp_addr = 0;
+    while(!sections[index].discovered.empty()){
+        // Get Address from Queue
+        uint64_t address = sections[index].discovered.front();
+        sections[index].discovered.pop();
 
+        // Seek to the Address
+        myself.pc = address;
+        myself.code = (uint8_t *)((uint8_t *)sections[index].data + address);
+        myself.code_size = sections[index].data_size + address;
 
-        }else {
-            //pthread_mutex_unlock(&NET_PTHREAD_MUTEX_UPDATE_VICTIMS);
-            //Sleep? WaitForSingleObject windows
+        // Set Address as Visited
+        sections[index].visited[address] = 0;
+
+        // Check if Function or Block Address
+        bool block = IsBlock(address, index);
+        bool function = IsFunction(address, index);
+
+        // Function Decompiler Logic
+        while (true){
+            // If End of Data Break
+            if (myself.pc >= sections[index].data_size) {
+                break;
+            }
+            // Disassemble Instruction
+            bool result = cs_disasm_iter(myself.handle, &myself.code, &myself.code_size, &myself.pc, insn);
+            // If Instruction Invalid
+            if (result == false){
+                myself.pc++;
+                myself.code = (uint8_t *)((uint8_t *)sections[index].data + myself.pc);
+                myself.code_size = sections[index].data_size + myself.pc;
+                printf("INVALID INSTRUCTION\n");
+                // Append Wildcard Bytes to Both Block and Function Trait
+                continue;
+            }
+            // If More Executable Data Add to Queue
+            if (result == true && IsEndInsn(insn) == true && myself.pc < sections[index].data_size){
+                tmp_addr = myself.pc+sizeof(insn->bytes);
+                if (IsVisited(tmp_addr, index) == false && tmp_addr < sections[index].data_size){
+                    sections[index].discovered.push(tmp_addr);
+                    sections[index].addresses[tmp_addr] = DECOMPILER_REV_OPERAND_TYPE_FUNCTION;
+                }
+            }
+            // Collect Instruction Operands
+            CollectInsn(insn, index);
+
+            // Debug Print Instruction
+            printf("address=0x%" PRIx64 ",block=%d,function=%d,queue=%ld,instruction=%s\t%s\n", insn->address,IsBlock(insn->address, index), IsFunction(insn->address, index), sections[index].discovered.size(), insn->mnemonic, insn->op_str);
+
+            // Collect Block and Function Data Here
+
+            // If Function and End of Function Break
+            if (function == true && IsEndInsn(insn) == true){
+                if (block == true && IsConditionalInsn(insn) == true){
+                    // End Block Data
+                    printf("END\n");
+                    break;
+                }
+                // End Function Data
+                printf("END\n");
+                break;
+            }
+            // If Block and End of Block Break
+            if (block == true && (IsConditionalInsn(insn) == true || IsEndInsn(insn) == true) && function == false){
+                // End Block Data
+                printf("END\n");
+                break;
+            }
         }
     }
-    
+    printf("END Program\n");
+    cs_free(insn, 1);
+    cs_close(&myself.handle);
+    return true;
 }
 uint DecompilerREV::Decompile(void* data, size_t data_size, size_t data_offset, uint index) {
     // struct * number of workers
@@ -122,53 +188,61 @@ uint DecompilerREV::Decompile(void* data, size_t data_size, size_t data_offset, 
     sections[index].data = data;
     sections[index].data_size = data_size;
     sections[index].code_size = data_size;
-   
-    cs_insn* insn = cs_malloc(sections[index].handle);
-    uint64_t tmp_addr = 0;
-    // Set First Instruction as Function which is also start of a block
+
+    // cs_insn* insn = cs_malloc(sections[index].handle);
+    // uint64_t tmp_addr = 0;
+
+    // Push First Address to Queue as Function
+    sections[index].discovered.push(0);
     sections[index].addresses[0] = DECOMPILER_REV_OPERAND_TYPE_FUNCTION;
-    while (true) {
-        if (sections[index].pc >= data_size && sections[index].discovered.empty()) {
-            break;
-        }
-        if (sections[index].pc >= data_size && !sections[index].discovered.empty()){
-            tmp_addr = sections[index].discovered.front();
-            sections[index].discovered.pop();
-            Seek(tmp_addr, data_size, index);
-            continue;
-        }
-        bool result = cs_disasm_iter(sections[index].handle, &sections[index].code, &sections[index].code_size, &sections[index].pc, insn);
-        if (result == false){
-            // Handle Invalid Instructions
-            Seek(sections[index].pc+1, data_size, index);
-            continue;
-        }
-        if (result == true && IsEndInsn(insn) == true && sections[index].pc < data_size){
-            // If More Executable Data Add to Queue
-            if (IsVisited(sections[index].pc+sizeof(insn->bytes), index) == false){
-                sections[index].visited[sections[index].pc+sizeof(insn->bytes)] = 0;
-                sections[index].discovered.push(sections[index].pc+sizeof(insn->bytes));
-            }
-        }
-        uint operand_type = CollectInsn(insn, index);
-        if (IsEndInsn(insn) == true) {
-            if (!sections[index].discovered.empty()) {
-                tmp_addr = sections[index].discovered.front();
-                sections[index].discovered.pop();
-                Seek(tmp_addr, data_size, index);
-            }
-        }
-        printf("address=0x%" PRIx64 ",block=%d,function=%d,queue=%ld,instruction=%s\t%s\n", insn->address,IsBlock(insn->address, index), IsFunction(insn->address, index), sections[index].discovered.size(), insn->mnemonic, insn->op_str);
-    }
-    cs_free(insn, 1);
-    return sections[index].pc;
+
+    // Start Worker
+    Worker(index);
+    // while (true) {
+    //     if (sections[index].pc >= data_size && sections[index].discovered.empty()) {
+    //         break;
+    //     }
+    //     if (sections[index].pc >= data_size && !sections[index].discovered.empty()){
+    //         tmp_addr = sections[index].discovered.front();
+    //         sections[index].discovered.pop();
+    //         Seek(tmp_addr, data_size, index);
+    //         continue;
+    //     }
+    //     bool result = cs_disasm_iter(sections[index].handle, &sections[index].code, &sections[index].code_size, &sections[index].pc, insn);
+    //     if (result == false){
+    //         // Handle Invalid Instructions
+    //         Seek(sections[index].pc+1, data_size, index);
+    //         continue;
+    //     }
+    //     if (result == true && IsEndInsn(insn) == true && sections[index].pc < data_size){
+    //         // If More Executable Data Add to Queue as Function if not Visited
+    //         tmp_addr = sections[index].pc+sizeof(insn->bytes);
+    //         if (IsVisited(tmp_addr, index) == false){
+    //             sections[index].visited[tmp_addr] = 0;
+    //             sections[index].discovered.push(tmp_addr);
+    //             sections[index].addresses[tmp_addr] = DECOMPILER_REV_OPERAND_TYPE_FUNCTION;
+    //         }
+    //     }
+    //     uint operand_type = CollectInsn(insn, index);
+    //     if (IsEndInsn(insn) == true) {
+    //         if (!sections[index].discovered.empty()) {
+    //             tmp_addr = sections[index].discovered.front();
+    //             sections[index].discovered.pop();
+    //             Seek(tmp_addr, data_size, index);
+    //         }
+    //     }
+    //     printf("address=0x%" PRIx64 ",block=%d,function=%d,queue=%ld,instruction=%s\t%s\n", insn->address,IsBlock(insn->address, index), IsFunction(insn->address, index), sections[index].discovered.size(), insn->mnemonic, insn->op_str);
+    // }
+    // cs_free(insn, 1);
+    // return sections[index].pc;
+    return 0;
 }
 
 bool DecompilerREV::IsVisited(uint64_t address, uint index) {
     return sections[index].visited.find(address) != sections[index].visited.end();
 }
 
-bool DecompilerREV::IsEndInsn(cs_insn* insn) {
+bool DecompilerREV::IsEndInsn(cs_insn *insn) {
     switch (insn->id) {
     case X86_INS_RET:
         return true;
@@ -181,6 +255,54 @@ bool DecompilerREV::IsEndInsn(cs_insn* insn) {
     case X86_INS_IRETD:
         return true;
     case X86_INS_IRETQ:
+        return true;
+    default:
+        break;
+    }
+    return false;
+}
+
+bool DecompilerREV::IsConditionalInsn(cs_insn* insn) {
+    switch (insn->id) {
+    case X86_INS_JMP:
+        return true;
+    case X86_INS_JNE:
+        return true;
+    case X86_INS_JNO:
+        return true;
+    case X86_INS_JNP:
+        return true;
+    case X86_INS_JL:
+        return true;
+    case X86_INS_JLE:
+        return true;
+    case X86_INS_JG:
+        return true;
+    case X86_INS_JGE:
+        return true;
+    case X86_INS_JE:
+        return true;
+    case X86_INS_JECXZ:
+        return true;
+    case X86_INS_JCXZ:
+        return true;
+    case X86_INS_JB:
+        return true;
+    case X86_INS_JBE:
+        return true;
+    case X86_INS_JA:
+        return true;
+    case X86_INS_JAE:
+        return true;
+    case X86_INS_JNS:
+        return true;
+    case X86_INS_JO:
+        return true;
+    case X86_INS_JP:
+        return true;
+    case X86_INS_JRCXZ:
+        return true;
+    case X86_INS_JS:
         return true;
     default:
         break;
@@ -283,7 +405,7 @@ uint DecompilerREV::CollectInsn(cs_insn* insn, uint index) {
 
 void DecompilerREV::CollectOperands(cs_insn* insn, int operand_type, uint index) {
     uint64_t address = X86_REL_ADDR(*insn);
-    if (!IsVisited(address, index)) {
+    if (IsVisited(address, index) == false && address < sections[index].data_size) {
         sections[index].visited[address] = 0;
         switch(operand_type){
             case DECOMPILER_REV_OPERAND_TYPE_BLOCK:
