d7bd57a4b0e45be74c4874308a07b134b15a5e86
c3rb3ru5 <c3rb3ru5@aabbcc.hopto.org>
2022-01-04 22:41:26 -0400

    bindings

--- bindings/python/pe.cpp
+++ bindings/python/pe.cpp
@@ -6,10 +6,23 @@
 namespace py = pybind11;
 
 void init_pe(py::module &handle){
-  py::class_<binlex::PE>(handle, "PE")
+  py::class_<binlex::PE>(handle, "PE", "Binlex PE Module")
   .def(py::init<>())
   .def("setup", &binlex::PE::Setup)
   .def("read_file", &binlex::PE::ReadFile)
+  .def("get_sections", [](binlex::PE &module){
+        auto result = py::list();
+        for (int i = 0; i < PE_MAX_SECTIONS; i++){
+            if (module.sections[i].data != NULL){
+                auto dict = py::dict();
+                dict["size"] = module.sections[i].size;
+                dict["data"] = py::bytes((char *)module.sections[i].data, module.sections[i].size);
+                dict["offset"] = module.sections[i].offset;
+                result.append(dict);
+            }
+        }
+        return result;
+    })
   .def("read_buffer", &binlex::PE::ReadBuffer);
   py::enum_<LIEF::PE::MACHINE_TYPES>(handle, "MACHINE_TYPES")
   .value("IMAGE_FILE_MACHINE_I386", LIEF::PE::MACHINE_TYPES::IMAGE_FILE_MACHINE_I386)
