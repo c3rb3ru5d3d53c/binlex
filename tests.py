0f7099ab6846466b0836d1af00e11aff0e315afd
c3rb3ru5 <c3rb3ru5@aabbcc.hopto.org>
2022-01-05 01:00:40 -0400

    bindings

--- tests.py
+++ tests.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
 
 from pprint import pprint
+from hashlib import sha256
 import build.pybinlex as pybinlex
 
 raw = pybinlex.Raw()
 raw.read_file('tests/raw/raw.x86', 0)
-#print(raw.get_sections())
+sections = raw.get_sections()
+pprint(sections)
 
 pe = pybinlex.PE()
 pe.setup(pybinlex.MACHINE_TYPES.IMAGE_FILE_MACHINE_I386)
 pe.read_file('tests/pe/pe.x86')
 sections = pe.get_sections()
-#print(sections)
+pprint(sections)
 
 elf = pybinlex.ELF()
 elf.setup(pybinlex.ARCH.EM_386)
 elf.read_file('tests/elf/elf.x86')
 sections = elf.get_sections()
 pprint(sections)
+
+decompiler = pybinlex.Decompiler()
+decompiler.setup(pybinlex.cs_arch.CS_ARCH_X86, pybinlex.cs_mode.CS_MODE_32, 0)
+print(decompiler.decompile(b'HelloWorld'))
