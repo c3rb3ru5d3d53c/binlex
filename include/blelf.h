efb1b821f05b83a107929855fabd35d27a3be757
sophia-brandt <scbrand@team2.3bf52cwkd32etotxc3j0ckc1ie.ux.internal.cloudapp.net>
2022-04-29 18:55:22 +0000

    Missing blelf.h file

--- include/blelf.h
+++ include/blelf.h
@@ -1,3 +1,6 @@
+#ifndef ELF_H
+#define ELF_H
+
 #include <iostream>
 #include <memory>
 #include <vector>
@@ -5,10 +8,11 @@
 #include <LIEF/ELF.hpp>
 #include "common.h"
 
-#ifndef ELF_H
-#define ELF_H
-
-#define ELF_MAX_SECTIONS 256
+#ifdef _WIN32
+#define BINLEX_EXPORT __declspec(dllexport)
+#else
+#define BINLEX_EXPORT 
+#endif
 
 using namespace std;
 using namespace LIEF::ELF;
@@ -16,7 +20,7 @@ using namespace LIEF::ELF;
 namespace binlex{
     class ELF : public Common{
         private:
-            void ParseSections();
+            bool ParseSections();
         public:
             ARCH mode = ARCH::EM_NONE;
             unique_ptr<LIEF::ELF::Binary> binary;
@@ -26,12 +30,14 @@ namespace binlex{
                 void *data;
                 set<uint64_t> functions;
             };
-            struct Section sections[ELF_MAX_SECTIONS];
-            ELF();
-            bool Setup(ARCH input_mode);
-            bool ReadFile(char *file_path);
-            bool ReadBuffer(void *data, size_t size);
-            ~ELF();
+            struct Section sections[BINARY_MAX_SECTIONS];
+            uint32_t total_exec_sections;
+
+            BINLEX_EXPORT ELF();
+            BINLEX_EXPORT bool Setup(ARCH input_mode);
+            BINLEX_EXPORT bool ReadFile(char *file_path);
+            BINLEX_EXPORT bool ReadBuffer(void *data, size_t size);
+            BINLEX_EXPORT ~ELF();
     };
 };
 
