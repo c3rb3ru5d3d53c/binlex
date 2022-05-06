f55571e55e2c9bb88fb411a4bdf7ac3b0d3b74f5
c3rb3ru5 <c3rb3ru5@aabbcc.hopto.org>
2022-05-06 06:29:06 -0300

    update testing script

--- tests/tests.py
+++ tests/tests.py
@@ -33,23 +33,23 @@ elf_sections = elf.get_sections()
 pprint(elf_sections)
 
 decompiler = pybinlex.Decompiler(raw)
-decompiler.setup(pybinlex.cs_arch.CS_ARCH_X86, pybinlex.cs_mode.CS_MODE_32, 0)
+decompiler.setup(pybinlex.cs_arch.CS_ARCH_X86, pybinlex.cs_mode.CS_MODE_32)
 decompiler.append_queue({0}, pybinlex.DECOMPILER_OPERAND_TYPE.DECOMPILER_OPERAND_TYPE_FUNCTION, 0)
 decompiler.decompile(raw_sections[0]['data'], raw_sections[0]['offset'], 0)
 traits = decompiler.get_traits()
 print(json.dumps(traits, indent=4))
 
 decompiler = pybinlex.Decompiler(elf)
+decompiler.setup(pybinlex.cs_arch.CS_ARCH_X86, pybinlex.cs_mode.CS_MODE_32)
 for i in range(0, len(elf_sections)):
-    decompiler.setup(pybinlex.cs_arch.CS_ARCH_X86, pybinlex.cs_mode.CS_MODE_32, i)
     decompiler.append_queue(elf_sections[i]['functions'], pybinlex.DECOMPILER_OPERAND_TYPE.DECOMPILER_OPERAND_TYPE_FUNCTION, i)
     decompiler.decompile(elf_sections[i]['data'], elf_sections[i]['offset'], i)
 traits = decompiler.get_traits()
 print(json.dumps(traits, indent=4))
 
 decompiler = pybinlex.Decompiler(pe)
+decompiler.setup(pybinlex.cs_arch.CS_ARCH_X86, pybinlex.cs_mode.CS_MODE_32)
 for i in range(0, len(pe_sections)):
-    decompiler.setup(pybinlex.cs_arch.CS_ARCH_X86, pybinlex.cs_mode.CS_MODE_32, i)
     decompiler.append_queue(pe_sections[i]['functions'], pybinlex.DECOMPILER_OPERAND_TYPE.DECOMPILER_OPERAND_TYPE_FUNCTION, i)
     decompiler.decompile(pe_sections[i]['data'], pe_sections[i]['offset'], i)
 traits = decompiler.get_traits()
