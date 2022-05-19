954451c587105f23610a8b97ecfefea079a1d88e
c3rb3ru5d3d53c <c3rb3ru5d3d53c@gmail.com>
2022-05-19 17:52:37 -0300

    remove cil setup

--- src/auto.cpp
+++ src/auto.cpp
@@ -66,7 +66,6 @@ int AutoLex::ProcessFile(char *file_path){
             int si = 0;
             for (auto section : pe._sections) {
                 if (section.offset == 0) continue;
-                if (cil_decompiler.Setup(CIL_DECOMPILER_TYPE_ALL) == false) return 1;
                 if (cil_decompiler.Decompile(section.data, section.size, si) == false) continue;
 	            si++;
             }
