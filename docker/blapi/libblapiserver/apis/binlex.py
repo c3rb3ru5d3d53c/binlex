5c794a915753c397e9f9a82f80a0d5920e2c44ba
m0n4 <unholyman@hotmail.fr>
2022-07-24 00:19:54 +0200

    Update binlex.py

--- docker/blapi/libblapiserver/apis/binlex.py
+++ docker/blapi/libblapiserver/apis/binlex.py
@@ -86,9 +86,9 @@ class binlex_samples_upload(Resource):
             return {
                 'status': 'processing'
             }
-        except Exception:
+        except Exception as e:
             return {
-                'error': 'Failed to add to decompiler queue'
+                'error': f'Failed to add to decompiler queue: {e}'
             }, 500
 
 #Download sample
