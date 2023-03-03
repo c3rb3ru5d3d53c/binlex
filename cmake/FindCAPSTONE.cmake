e14f77cf215075ba38756c0868f2dba132c7c8b7
c3rb3ru5d3d53c <c3rb3ru5d3d53c@gmail.com>
2023-03-03 08:59:36 -0400

    wheel

--- cmake/FindCAPSTONE.cmake
+++ cmake/FindCAPSTONE.cmake
@@ -1,5 +1,6 @@
 set(CAPSTONE_ROOT "${CMAKE_SOURCE_DIR}/deps/build/capstone")
 set(CAPSTONE_INCLUDE_DIRS "${CAPSTONE_ROOT}/include")
+file(MAKE_DIRECTORY ${CAPSTONE_INCLUDE_DIRS})
 
 add_library(capstone_static STATIC IMPORTED)
 set_target_properties(capstone_static PROPERTIES
