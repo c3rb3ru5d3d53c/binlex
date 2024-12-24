f547f3d62143c5796db90d071d982a8fe0ea7ca4
c3rb3r3u5d3d53c <c3rb3ru5d3d53c@gmail.com>
2024-12-24 18:03:04 -0400

    python colormap [skip ci]

---
+++ src/bindings/python/src/imaging/mod.rs
@@ -0,0 +1,18 @@
+pub mod colormap;
+
+pub use colormap::ColorMap;
+use crate::imaging::colormap::colormap_init;
+
+use pyo3::{prelude::*, wrap_pymodule};
+
+#[pymodule]
+#[pyo3(name = "imaging")]
+pub fn imaging_init(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_wrapped(wrap_pymodule!(colormap_init))?;
+    m.add_class::<ColorMap>()?;
+    py.import_bound("sys")?
+        .getattr("modules")?
+        .set_item("binlex.imaging", m)?;
+    m.setattr("__name__", "binlex.imaging")?;
+    Ok(())
+}
