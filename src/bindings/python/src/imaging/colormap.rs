3f5891e9510d703a8d775b421c3d72eaef32b77a
c3rb3r3u5d3d53c <c3rb3ru5d3d53c@gmail.com>
2024-12-25 09:52:02 -0400

    ColorMapType Python Bindings

--- src/bindings/python/src/imaging/colormap.rs
+++ src/bindings/python/src/imaging/colormap.rs
@@ -1,10 +1,26 @@
 use pyo3::prelude::*;
-
 use binlex::imaging::colormap::ColorMap as InnerColorMap;
+use binlex::imaging::colormap::ColorMapType as InnerColorMapType;
 use std::sync::Arc;
 use std::sync::Mutex;
 use pyo3::exceptions::PyRuntimeError;
 use pyo3::types::PyBytes;
+
+#[pyclass]
+pub struct ColorMapType {
+    inner: Arc<Mutex<InnerColorMapType>>,
+}
+
+#[pymethods]
+impl ColorMapType {
+    #[staticmethod]
+    pub fn from_string(string: String) -> PyResult<Self> {
+        let inner = InnerColorMapType::from_string(&string).map_err(|e| PyErr::new::<PyRuntimeError, _>(e.to_string()))?;
+        Ok(Self {
+            inner: Arc::new(Mutex::new(inner))
+        })
+    }
+}
 
 #[pyclass]
 pub struct ColorMap {
@@ -21,8 +37,22 @@ impl ColorMap {
         }
     }
 
-    #[pyo3(text_signature = "($self, offset, data)")]
-    pub fn append(&mut self, py: Python, offset: u64, data: Py<PyBytes>) {
+    #[pyo3(text_signature = "($self, color_map_type)")]
+    pub fn set_color_map_type(&mut self, py: Python, color_map_type: Py<ColorMapType>) {
+        let inner_color_map_type = color_map_type
+            .borrow(py)
+            .inner
+            .lock()
+            .unwrap()
+            .clone();
+        self.inner
+            .lock()
+            .unwrap()
+            .set_color_map_type(inner_color_map_type);
+    }
+
+    #[pyo3(signature = (data, offset=0))]
+    pub fn append(&mut self, py: Python, data: Py<PyBytes>, offset: u64) {
         let inner_data = data.bind(py).as_bytes();
         self.inner.lock().unwrap().append(offset, inner_data);
     }
@@ -52,7 +82,8 @@ impl ColorMap {
         self.inner
             .lock()
             .unwrap()
-            .write(&file_path).map_err(|e| PyErr::new::<PyRuntimeError, _>(e.to_string()))
+            .write(&file_path)
+            .map_err(|e| PyErr::new::<PyRuntimeError, _>(e.to_string()))
     }
 }
 
@@ -60,6 +91,7 @@ impl ColorMap {
 #[pyo3(name = "colormap")]
 pub fn colormap_init(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<ColorMap>()?;
+    m.add_class::<ColorMapType>()?;
     py.import_bound("sys")?
         .getattr("modules")?
         .set_item("binlex.imaging.colormap", m)?;
--- src/imaging/mod.rs
+++ src/imaging/mod.rs
@@ -1,1 +1,4 @@
 pub mod colormap;
+
+pub use colormap::ColorMap;
+pub use colormap::ColorMapType;
