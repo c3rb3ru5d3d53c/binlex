0eb4f82d17deeea182eeebbbd9f438b9375a393b
c3rb3r3u5d3d53c <c3rb3ru5d3d53c@gmail.com>
2024-12-25 09:52:02 -0400

    ColorMapType Python Bindings

--- src/bindings/python/src/imaging/mod.rs
+++ src/bindings/python/src/imaging/mod.rs
@@ -1,6 +1,7 @@
 pub mod colormap;
 
 pub use colormap::ColorMap;
+pub use colormap::ColorMapType;
 use crate::imaging::colormap::colormap_init;
 
 use pyo3::{prelude::*, wrap_pymodule};
@@ -10,6 +11,7 @@ use pyo3::{prelude::*, wrap_pymodule};
 pub fn imaging_init(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_wrapped(wrap_pymodule!(colormap_init))?;
     m.add_class::<ColorMap>()?;
+    m.add_class::<ColorMapType>()?;
     py.import_bound("sys")?
         .getattr("modules")?
         .set_item("binlex.imaging", m)?;
