da54319e08a8968f468bb4cd4a189b502cd11671
c3rb3r3u5d3d53c <c3rb3ru5d3d53c@gmail.com>
2025-01-13 21:00:37 -0400

    HTTP Client [skip ci]

--- scripts/libblclient/libblclient/client.py
+++ scripts/libblclient/libblclient/client.py
@@ -1,22 +1,37 @@
+import json
 import requests
+import urllib3
 
-class BinlexHTTPClient():
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+class BLClient():
     def __init__(self, url: str, api_key: str, verify=False):
         self.url = url
         self.api_key = api_key
         self.verify = verify
 
-    def get_embeddings_databases(self):
+    def databases(self):
         r = requests.get(
             url=f'{self.url}/embeddings/databases',
             headers={
-                'API-Key', self.api_key
+                'API-Key': self.api_key
             },
             verify=self.verify,
         )
-        r.status_code, r.json()
+        return r.status_code, json.loads(r.content)
 
-    def get_embeddings_database_collections(self, dataase: str):
+    def inference(self, data: dict):
+        r = requests.post(
+            url=f'{self.url}/embeddings/inference',
+            headers={
+                'API-Key': self.api_key
+            },
+            json=data,
+            verify=self.verify,
+        )
+        return r.status_code, json.loads(r.content)
+
+    def collections(self, database: str):
         r = requests.get(
             url=f'{self.url}/embeddings/{database}/collections',
             headers={
@@ -24,36 +39,44 @@ class BinlexHTTPClient():
             },
             verify=self.verify,
         )
-        r.status_code, r.json()
+        return r.status_code, json.loads(r.content)
 
-    def get_embeddings_database_collection_partitins(self, dataase: str, collection: str):
+    def partitions(self, database: str, collection: str):
         r = requests.get(
             url=f'{self.url}/embeddings/{database}/{collection}/partitions',
             headers={
-                'API-Key', self.api_key
+                'API-Key': self.api_key
             },
             verify=self.verify,
         )
-        r.status_code, r.json()
+        return r.status_code, json.loads(r.content)
 
-    def post_embeddings_database_collction_partition_index(self, database: str, collection: str, partition: str, data: dict):
+    def index(self, database: str, collection: str, partition: str, data: dict):
         r = requests.post(
-            url=f'{self.url}/embeddings/{database}/{collection}/{partition}',
+            url=f'{self.url}/embeddings/{database}/{collection}/{partition}/index',
             headers={
-                'API-Key', self.api_key
+                'API-Key': self.api_key
             },
             json=data,
             verify=self.verify,
         )
-        r.status_code, r.json()
+        return r.status_code, json.loads(r.content)
 
-    def post_embeddings_database_collction_partition_search(self, database: str, collection: str, partition: str, vector: list):
+    def search(
+        self,
+        database: str,
+        collection: str,
+        partition: str,
+        offset: int,
+        limit: int,
+        threshold: float,
+        vector: dict):
         r = requests.post(
-            url=f'{self.url}/embeddings/{database}/{collection}/{partition}/search',
+            url=f'{self.url}/embeddings/{database}/{collection}/{partition}/search/{offset}/{limit}/{threshold}',
             headers={
-                'API-Key', self.api_key
+                'API-Key': self.api_key
             },
             json=vector,
             verify=self.verify,
         )
-        r.status_code, r.json()
+        return r.status_code, json.loads(r.json())
